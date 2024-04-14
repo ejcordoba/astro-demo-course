80b92daa461e8b3ab41333a6b864718beadf47d4
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2024-04-14 17:21:44 +0200

    Setup for static pages

--- src/services/spacex.ts
+++ src/services/spacex.ts
@@ -2,7 +2,7 @@ import { type Doc, type APISpaceXResponse } from "../types/api";
 
 export const getLaunchBy = async ({ id }: { id: string }) => {
   const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`);
-  const { launch } = (await res.json()) as Doc;
+  const launch = (await res.json()) as Doc;
   return launch;
 };
 
